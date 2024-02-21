f650006e698d535564e2cf7487c8bad4defb8716
Robert Lukas <robert_lukas@outlook.de>
2024-02-21 21:34:04 +0100

    update3

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -16,7 +16,7 @@ const Header = () => (
     </Div1>
     <Div2>
       <li>
-        <Link href="/PortfolioRL/#projects" passHref>
+        <Link href="/PortfolioRL/#project" passHref>
           <NavLink>Projects</NavLink>
         </Link>
       </li>
